d42fdcde19710318b1bde168422d03eb3eb6df7e
Ventrapragada Naga Venkata Sivasai Prajith <125337761+Jith2252@users.noreply.github.com>
2023-08-14 22:20:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /tmafilesbotv2
 fi
 cd /Auto-search-tamil-bot
 pip3 install -U -r requirements.txt
