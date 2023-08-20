480835ed87922df1441e5fcd36b6bb6e9652e278
Ventrapragada Naga Venkata Sivasai Prajith <125337761+Jith2252@users.noreply.github.com>
2023-08-20 13:06:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Jith2252/tmafilesbotv2.git /tmafilesbotv2
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /tmafilesbotv2
-fi
-cd /Auto-search-tamil-bot
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+python3 update.py && python3 bot.py
