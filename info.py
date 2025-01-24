85e848cf6d4789aec90325d776d50ff01eedf1dd
balayyatrolls <147257131+balayyatrolls@users.noreply.github.com>
2025-01-24 14:37:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Balayyatrolls:Balayyatrolls@cluster0.itks5pr.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Btmoviesbottt")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://moviesbot:moviesbot@movies.pv7ii.mongodb.net/?retryWrites=true&w=majority&appName=Movies")
+DATABASE_NAME = environ.get('DATABASE_NAME', "movies")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
